a058b5430a1fe4539ed074533b231742af1c16a4
KevinRaleie-dev <kkevraleie@gmail.com>
2020-12-01 14:35:22 +0200

    Section 2: set up the website

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { useHelloQuery } from './generated/graphql';
+
+function App() {
+
+  const { data, loading } = useHelloQuery();
+
+  if(loading || !data) {
+    return <p>loading...</p>
+  }
+
+  return (
+    <div className="App">
+      {data.hello}
+    </div>
+  );
+}
+
+export default App;
